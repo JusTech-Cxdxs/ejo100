183e2baccf285781dd1cb11d47f8a197075aa335
JusTech-Cxdxs <enefolajustice004@gmail.com>
2026-09-08 01:05:57 +0100

    Add files via upload

--- apps/website/app/news/page.tsx
+++ apps/website/app/news/page.tsx
@@ -1,5 +1,5 @@
 import { PagePlaceholder } from '@/components/PagePlaceholder';
 
 export default function Page() {
-  return <PagePlaceholder title="News" description="Company news and announcements." />;
+  return <PagePlaceholder title="News" description="Organisation news and announcements." />;
 }
